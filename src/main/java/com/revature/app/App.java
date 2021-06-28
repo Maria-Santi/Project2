6d3b364f5afd465b48f4d28f597768a1118e61b1
Maria-Santi <misantibanez7@gmail.com>
2021-06-28 16:36:17 -0500

    merged

--- src/main/java/com/revature/app/App.java
+++ src/main/java/com/revature/app/App.java
@@ -1,11 +1,16 @@
 package com.revature.app;
 
 import com.revature.controllers.OrderController;
+import com.revature.controllers.ProductController;
 import com.revature.daos.OrderDAO;
 import com.revature.daos.OrderDAOPostgres;
+import com.revature.daos.ProductDAO;
+import com.revature.daos.ProductDAOPostgres;
 import com.revature.entities.Order;
 import com.revature.services.OrderService;
 import com.revature.services.OrderServiceImpl;
+import com.revature.services.ProductService;
+import com.revature.services.ProductServiceImpl;
 import io.javalin.Javalin;
 
 public class App {
@@ -19,6 +24,19 @@ public class App {
         OrderDAO orderDAO = new OrderDAOPostgres();
         OrderService orderService = new OrderServiceImpl(orderDAO);
         OrderController orderController = new OrderController(orderService);
+
+        ProductDAO productDAO = new ProductDAOPostgres();
+        ProductService productService = new ProductServiceImpl(productDAO);
+        ProductController productController = new ProductController(productService);
+
+        app.get("/products", productController.getAllProducts);
+
+        app.get("/products/:id", productController.getProductById);
+
+        app.get("/products/name/:name", productController.getProductByName);
+
+        app.put("/products/:id", productController.updateProduct);
+
 
         app.post("/orders", orderController.placeOrder);
 
